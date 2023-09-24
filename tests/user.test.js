8e065268d8f2b6d507455884b0f249d3a015a5f8
Nopalogic <naufaladhi02@gmail.com>
2023-09-24 20:19:44 +0700

    test: Unit test for login User API

--- tests/user.test.js
+++ tests/user.test.js
@@ -3,56 +3,56 @@ import { logger } from '../src/app/logging.js';
 import { server } from '../src/app/server.js';
 import { createTestUser, removeTestUser } from './test-util.js';
 
-// describe('POST /api/users', () => {
-// 	afterEach(async () => {
-// 		await removeTestUser;
-// 	});
+describe('POST /api/users', () => {
+	afterEach(async () => {
+		await removeTestUser;
+	});
 
-// 	it('should can register new user', async () => {
-// 		const result = await supertest(server).post('/api/users').send({
-// 			username: 'test',
-// 			password: 'test1234',
-// 			name: 'test',
-// 		});
+	it('should can register new user', async () => {
+		const result = await supertest(server).post('/api/users').send({
+			username: 'test',
+			password: 'test1234',
+			name: 'test',
+		});
 
-// 		expect(result.status).toBe(200);
-// 		expect(result.body.data.username).toBe('test');
-// 		expect(result.body.data.name).toBe('test');
-// 		expect(result.body.data.password).toBeUndefined();
-// 	});
+		expect(result.status).toBe(200);
+		expect(result.body.data.username).toBe('test');
+		expect(result.body.data.name).toBe('test');
+		expect(result.body.data.password).toBeUndefined();
+	});
 
-// 	it('should reject if request is invalide', async () => {
-// 		const result = await supertest(server).post('/api/users').send({
-// 			username: '',
-// 			password: '',
-// 			name: '',
-// 		});
+	it('should reject if request is invalide', async () => {
+		const result = await supertest(server).post('/api/users').send({
+			username: '',
+			password: '',
+			name: '',
+		});
 
-// 		logger.info(result.body);
+		logger.info(result.body);
 
-// 		expect(result.status).toBe(400);
-// 		expect(result.body.errors).toBeDefined();
-// 	});
+		expect(result.status).toBe(400);
+		expect(result.body.errors).toBeDefined();
+	});
 
-// 	it('should reject if username already exist', async () => {
-// 		let result = await supertest(server).post('/api/users').send({
-// 			username: 'test',
-// 			password: 'test1234',
-// 			name: 'test',
-// 		});
+	it('should reject if username already exist', async () => {
+		let result = await supertest(server).post('/api/users').send({
+			username: 'test',
+			password: 'test1234',
+			name: 'test',
+		});
 
-// 		result = await supertest(server).post('/api/users').send({
-// 			username: 'test',
-// 			password: 'test1234',
-// 			name: 'test',
-// 		});
+		result = await supertest(server).post('/api/users').send({
+			username: 'test',
+			password: 'test1234',
+			name: 'test',
+		});
 
-// 		logger.info(result.body);
+		logger.info(result.body);
 
-// 		expect(result.status).toBe(400);
-// 		expect(result.body.errors).toBeDefined();
-// 	});
-// });
+		expect(result.status).toBe(400);
+		expect(result.body.errors).toBeDefined();
+	});
+});
 
 describe('POST /api/users/login', () => {
 	beforeEach(async () => {
@@ -73,6 +73,42 @@ describe('POST /api/users/login', () => {
 
 		expect(result.status).toBe(200);
 		expect(result.body.data.token).toBeDefined();
-		expect(result.body.token).not.toBeDefined('test');
+		expect(result.body.token).not.toBe('test');
+	});
+
+	it('should reject login if request invalid', async () => {
+		const result = await supertest(server).post('/api/users/login').send({
+			username: '',
+			password: '',
+		});
+
+		logger.info(result.body);
+
+		expect(result.status).toBe(400);
+		expect(result.body.errors).toBeDefined();
+	});
+
+	it('should reject login if password is wrong', async () => {
+		const result = await supertest(server).post('/api/users/login').send({
+			username: 'test',
+			password: 'tst1234',
+		});
+
+		logger.info(result.body);
+
+		expect(result.status).toBe(401);
+		expect(result.body.errors).toBeDefined();
+	});
+
+	it('should reject login if username is wrong', async () => {
+		const result = await supertest(server).post('/api/users/login').send({
+			username: 'tst',
+			password: 'test1234',
+		});
+
+		logger.info(result.body);
+
+		expect(result.status).toBe(401);
+		expect(result.body.errors).toBeDefined();
 	});
 });
